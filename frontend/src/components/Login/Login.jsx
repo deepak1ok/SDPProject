fdaa47226d7587c75e28001b2f8b2ff00dd0616f
Deepak Kumar <deepak.kr10460@gmail.com>
2024-03-19 09:45:39 +0530

    update login page

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = "http://localhost:3000/api/auth";
+      const url = "http://localhost:3000/api/users/auth";
       const { data: res } = await axios.post(url, data);
       localStorage.setItem("token", res.data);
       window.location = "/main";
